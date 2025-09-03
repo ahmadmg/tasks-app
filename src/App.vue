1cd8e64525ae353fff178984cee9cb23dabd0439
ahmadmg <ahmadmg@gmail.com>
2025-09-03 13:44:53 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import TaskForm from './components/TaskForm.vue'
+const message = ref("Tasks App");
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+  <TaskForm />
+  </main>
+</template>
+
+<style >
+main {
+  padding: 2rem;
+  text-align: center;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
